8bc04412bed6a4c5cfc8b47510f6d8c381d48145
Yash Deshmukh <yashvd2006@gmail.com>
2025-09-21 15:21:28 +0530

    Add files via upload

---
+++ assi-1tutedude.py
@@ -0,0 +1,26 @@
+a = int(input("enter 1st number:"))
+b = int(input("enter 2nd number:"))
+
+sum = a+b
+sub = a-b
+mul = a*b
+div = a/b
+
+
+print("addition is",sum)
+print("subtraction is",sub)
+print("multiplication is",mul)
+print("division is",div)
+
+
+
+
+#Question 2nd concatenation of name
+
+first_name = input("Enter First Name;")
+last_name = input("Enter Second Name;")
+
+full_name = first_name + last_name
+print("Hello",full_name)
+
+
