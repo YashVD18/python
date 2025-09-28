9cb09eb9ce1c45a0656f37a3aad3af52c8213949
Yash Deshmukh <yashvd2006@gmail.com>
2025-09-28 18:55:51 +0530

    Add files via upload

---
+++ assi-2tutedude.py
@@ -0,0 +1,22 @@
+#Task 1
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Number is Even")   
+else:
+    print("Number is Odd")  
+
+
+#Task 2
+
+# Step 1 & 2: Use for loop and calculate sum
+total = 0
+for i in range(1, 51):   # goes from 1 to 50
+    total += i
+
+# Step 3: Display the final sum
+print("The sum of numbers from 1 to 50 is:", total)
+
+
+
+
